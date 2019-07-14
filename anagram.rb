e37e8123cd0b47bd57f4ac362b202070b4be5f13
Daniela <danielavitosova@seznam.cz>
2019-07-14 17:21:03 +0200

    3 letters word algorithm - v2

--- anagram.rb
+++ anagram.rb
@@ -1,34 +1,18 @@
 require_relative 'reverse_letters'
 
-# my_word = ["a","b","c"]
+def find_anagram(letters)
+  length = letters.length
 
-def find_anagram(word)
-  word2 = reverse_letters(word)
-  word1 = word[0]+word[1]+word[2]
-  word2 = word2[0]+word2[1]+word2[2]
+  letters = letters.split("")
 
-  word3 = []
+  anagrams = []
 
-  word3 << word[0]
-  word3 << word[2]
-  word3 << word[1]
+  letters.each_with_index do |letter, index|
+    remaining_letters = letters.select {|l| l != letter}
+    anagrams << letter + remaining_letters.join
 
-  word4 = reverse_letters(word3)
-  word3 = word[0]+word[2]+word[1]
-  word4 = word4[0]+word4[1]+word4[2]
+    anagrams << letter + reverse_letters(remaining_letters).join
+  end
 
-  word5 = []
-
-  word5 << word[1]
-  word5 << word[0]
-  word5 << word[2]
-
-  word6 = reverse_letters(word5)
-  word5 = word[1]+word[0]+word[2]
-  word6 = word6[0]+word6[1]+word6[2]
-
-  anagram = [word1,word2,word3,word4,word5,word6]
-  print anagram
+  anagrams
 end
-
-# find_anagram(my_word)
